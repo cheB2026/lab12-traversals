0d1f442169810e29bdc5794fa83fe397a518218d
Frank Lo <franklo1471@gmail.com>
2023-04-28 09:51:09 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -242,6 +242,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			while(!post.isEmpty()) {
 				BSTNode<T> node = post.pop();
 				System.out.print(node + " ");
+				//test
 			}
 		}
 
@@ -270,8 +271,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.insert(2);
 		bst.insert(8);
 		bst.insert(15);
-		//bst.insert(10);
-		//bst.insert(3);
+		bst.insert(10);
+		bst.insert(3);
 		System.out.println(bst);
 	
 		System.out.println("In Order Traversals");
